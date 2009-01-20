cefa3ec998e5c0987375e4e790395726cc77bcf6
madarche <devnull@localhost>
2009-01-20 11:01:37 +0000

    bundleman init.

--- DistantPublisher.py
+++ DistantPublisher.py
@@ -16,6 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
+# $Id$
 """
     DistantPublisher can be hooked to a portal to provide
     distant publishing APIs, based on CPSRemoteController
--- PartialPublisherStackElement.py
+++ PartialPublisherStackElement.py
@@ -16,6 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
+# $Id$
 from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
 
